1b8ab6c1b02a8db9aabf4dabe466ddb8295d442e
Max Bradley <max.milian.bradley@gmail.com>
2018-03-04 20:01:19 +0000

    Completed StringPropertiesAndMethods Lecture

---
+++ ObjectAndDataStructures/StringPropertiesAndMethods.py
@@ -0,0 +1,42 @@
+name = 'Sam'
+
+#name[0] = 'P' throws an error because strings are immutable
+
+last_letters = name[1:]
+
+print('P' + last_letters)
+
+
+x = 'Hello World'
+
+print(x + ' it is beautiful outside')
+
+x = x + " it's a lovely day!"
+
+print(x)
+
+
+letter = 'z'
+
+print(letter * 10)
+
+
+print(2 + 3)
+
+print('2' + '3')
+
+
+x = 'Hello World'
+
+print(x.upper())
+
+print(x.lower())
+
+print(x.split())
+
+x = 'Hi this is a string'
+
+print(x.split())
+
+print(x.split('i'))
+
