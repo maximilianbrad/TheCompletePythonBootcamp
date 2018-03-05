f5fb5df933b6ddcd595b14e2aff9f240716d28bc
Max Bradley <max.milian.bradley@gmail.com>
2018-03-05 14:57:48 +0000

    Completed Lists Lecture

--- S3_ObjectAndDataStructures/L21_Lists.py
+++ S3_ObjectAndDataStructures/L21_Lists.py
@@ -11,3 +11,47 @@ print(myList[0])
 
 print(myList[1:])
 
+#Concatenation
+anotherList = ['four', 'five']
+
+print(myList + anotherList)
+
+print(myList, anotherList)
+
+newList = myList + anotherList
+
+print(newList)
+
+#Lists are MUTABLE
+newList[0] = 'ONE ALL CAPS'
+
+print(newList)
+
+#list methods
+newList.append('six')
+
+print(newList)
+
+print(newList.pop()) #function returns popped item
+
+print(newList)
+
+poppedItem = newList.pop()
+
+print(poppedItem)
+
+print(newList.pop(0))
+
+print(newList)
+
+newList = ['a', 'd', 'r', 'o', 'b']
+numList = [1,2,3,4]
+
+newList.sort() #sorts list items IN PLACE ie. returns None
+print(newList)
+
+numList.sort()
+print(numList)
+
+numList.reverse()
+print(numList)
