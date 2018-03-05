7113c9bbd7f395614d06dbac1ab878b5dd629778
Max Bradley <max.milian.bradley@gmail.com>
2018-03-05 14:39:56 +0000

    Changed names of Folders and Files so that they are easier to order

---
+++ S3_ObjectAndDataStructures/L21_Lists.py
@@ -0,0 +1,13 @@
+myList = [1,2,3]
+
+myList = ['String', 100, 23.1]
+
+print(len(myList))
+
+myList = ['one', 'two', 'three']
+
+#Indexing and Slicing work the same for lists as strings
+print(myList[0])
+
+print(myList[1:])
+
