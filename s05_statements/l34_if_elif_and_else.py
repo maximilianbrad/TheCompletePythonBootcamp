2b3d5aff7c6aa8d3e72cc885b9869909e3900b3b
Max Bradley <max.milian.bradley@gmail.com>
2018-03-07 14:30:14 +0000

    completed if_elif_and_else lecture

---
+++ s05_statements/l34_if_elif_and_else.py
@@ -0,0 +1,26 @@
+hungry = False
+
+if hungry:
+    print('Feed me!')
+else:
+    print('Im not hungry')
+
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print("Cars are cool!")
+elif loc == 'Bank':
+    print("Money is cool")
+elif loc == 'Store':
+    print("Welcome to the store")
+else:
+    print("I do not know much")
+
+name = 'Sammy'
+
+if name == 'Frankie':
+    print("Hello Frankie")
+elif name == 'Sammy':
+    print("Hello Sammy")
+else:
+    print("What is your name?")
