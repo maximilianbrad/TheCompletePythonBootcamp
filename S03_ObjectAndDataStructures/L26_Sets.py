149d788860585ebdd445f00eb766c875317cfeff
Max Bradley <max.milian.bradley@gmail.com>
2018-03-07 11:35:05 +0000

    Completed Sets Lecture

---
+++ S03_ObjectAndDataStructures/L26_Sets.py
@@ -0,0 +1,19 @@
+mySet = set()
+
+print(mySet)
+
+mySet.add(1)
+
+print(mySet)
+
+mySet.add(2)
+
+print(mySet)
+
+mySet.add(2)
+
+print(mySet)
+
+myList = [1,1,1,1,1,1,1,12,2,2,2,2,33,3,3,3]
+
+print(set(myList))
